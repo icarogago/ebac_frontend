f53db515b8d28698d90e36063d95a6f374032728
Icaro Gago <148130229+icarogago@users.noreply.github.com>
2024-09-24 20:19:16 -0300

    Add files via upload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,107 @@
 module.exports = function(grunt){
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg:grunt.file.readJSON('package.json'),
         less:{
             development:{
                 files:{
-                    'main.css':'main.less'
+                    'dev/styles/main.css': 'src/styles/main.less'
                 }
-            }            
+            },
+            production:{
+                options:{
+                    compress: true,
+                },
+                files:{
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
         },
+        watch:{
+            less:{
+                files:['src/styles/**/*.less'],
+                tasks:['less:development']
+            },
+            html:{
+                files:['src/index.html'],
+                tasks:['replace:dev']
+            }
+        },
+        replace:{
+            dev:{
+                options:{
+                    patterns:[
+                        {
+                            match:'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        },
+                        {
+                            match:'ENDERECO_DO_JS',
+                            replacement: '../src/scripts/main.js'
+                        }
+                    ]
+                },
+                files:[
+                    {
+                        expand:true,
+                        flatten:true,
+                        src:['src/index.html'],
+                        dest:'dev/'
+                    }
+                ]
+            },
+            dist:{
+                options:{
+                    patterns:[
+                        {
+                            match:'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        },
+                        {
+                            match:'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
+                        }
+                    ]
+                },
+                files:[
+                    {
+                        expand:true,
+                        flatten:true,
+                        src:['prebuild/index.html'],
+                        dest:'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin:{
+            dist:{
+                options:{
+                    removeComments:true,
+                    collapseWhitespace:true,
+                },
+                files:{
+                    'prebuild/index.html': 'src/index.html'
+                }
+            }
+        },
+        clean:['prebuild'],
         uglify:{
             target:{
                 files:{
-                    'main.min.js':'main.js'
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
                 }
             }
         }
     })
 
+
+    grunt.loadNpmTasks('grunt-contrib-less')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-replace')
+    grunt.loadNpmTasks('grunt-contrib-htmlmin')
+    grunt.loadNpmTasks('grunt-contrib-clean')
+    grunt.loadNpmTasks('grunt-contrib-uglify')
     
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['less','uglify']);
-
+    grunt.registerTask('default',['watch'])
+    grunt.registerTask('build',['less:production','htmlmin:dist','replace:dist','clean','uglify'])
 }
